c3e769405970699a261fb7ccb6686e7567438b96
Bikramjeet singh <bs863313@gmail.com>
2023-12-09 23:58:06 +0530

    all todo apis creted successfully

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -3,8 +3,103 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todos;
 
 class TodosController extends Controller
 {
-    //
+    // Get all todos
+    public function getAllTodos()
+    {
+        $todos = Todos::all();
+        return response()->json($todos);
+    }
+
+    // Get a single todo by ID
+    public function getTodoById($id)
+    {
+        $todo = Todos::find($id);
+
+        if (!$todo) {
+            return response()->json(['message' => 'Todo not found'], 404);
+        }
+
+        return response()->json($todo);
+    }
+
+    // Create a new todo
+    public function createTodo(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string',     
+        ]);
+
+        $todo = Todos::create($request->all());
+    if($todo){
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Todo created successfully',
+            'data' => $todo,
+        ], 201);
+    }else{
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Failed to create todo',
+        ], 500);
+    }
+    }
+
+    // Update an existing todo
+    public function updateTodo(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'string',           
+        ]);
+
+        $todo = Todos::find($id);
+
+        if (!$todo) {
+            return response()->json(['message' => 'Todo not found'], 404);
+        }
+
+        $todo->update($request->all());
+        if($todo){
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Todo updated successfully',
+                'data' => $todo,
+            ], 200);
+        }else{
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Failed to update todo',
+            ], 500);
+        }
+
+       
+    }
+
+    // Delete a todo
+    public function deleteTodo($id)
+    {
+        $todo = Todos::find($id);
+
+        if (!$todo) {
+            return response()->json(['message' => 'Todo not found'], 404);
+        }
+
+        $todo->delete();
+        if($todo){
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Todo deleted successfully',
+            ], 200);
+        }else{
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Failed to delete todo',
+            ], 500);
+        }
+
+       
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+use App\Http\Controllers\TodosController;
+
+Route::get('/todos', [TodosController::class, 'getAllTodos']);
+Route::get('/todos/{id}', [TodosController::class, 'getTodoById']);
+Route::post('/todos', [TodosController::class, 'createTodo']);
+Route::put('/todos/{id}', [TodosController::class, 'updateTodo']);
+Route::delete('/todos/{id}', [TodosController::class, 'deleteTodo']);
+
+
